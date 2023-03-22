06e54938578b82b324d06905bbdd972ead6e917a
NDD53 <126294583+NDD53@users.noreply.github.com>
2023-03-22 15:41:54 +0000

    All the solutions to the puzzle as 1s or -1s

--- unit10/PuzzleSolutions.java
+++ unit10/PuzzleSolutions.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 
 public class PuzzleSolutions {
     public static int[][] puzzler(int[] arr,int n) {
+        int count = 0;
         String finbin = "";
         int[] fin = new int[arr.length];
         for(int k = 0; k < Math.pow(2,arr.length);k++){
@@ -25,11 +26,11 @@ public class PuzzleSolutions {
             }
             if(j==n){
                 finbin += bin;
+                count++;
             }
-
         }
-        int[][] ret = new int[finbin.length()/arr.length][arr.length];
-        for(int row = 0; row < finbin.length()/arr.length; row++){
+        int[][] ret = new int[count][arr.length];
+        for(int row = 0; row < count; row++){
             for(int col = 0; col < arr.length; col++){
                 if(finbin.substring(0, 1).equals("1")){
                     ret[row][col] = 1;
@@ -38,17 +39,22 @@ public class PuzzleSolutions {
                     ret[row][col] = -1;
                 }
                 finbin = finbin.substring(1);
-
             }
         }
-
-        return ret;
-            
+        return ret;   
     }
 
     public static void main(String[] args) {
         System.out.println("");
-        int[] arr = {4, 10, 6, 10, 4, 2};
-        System.out.println(Arrays.deepToString(puzzler(arr,0)));
+        int target = 20;
+        int[] arr = {4, 10, 1, 6, 10, 9, 4, 2};
+        int[][] pri = puzzler(arr,target);
+        System.out.println("Target number: " + target);
+        System.out.println("Initial array:");
+        System.out.println(Arrays.toString(arr));
+        System.out.println("List of all solutions:");
+        for(int[] row : pri){
+            System.out.println(Arrays.toString(row));
+        }
     }
 }
