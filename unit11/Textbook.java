fe21e10dced7c50f3e3ebfdaf2dea0c73ac0e8ba
NDD53 <126294583+NDD53@users.noreply.github.com>
2023-04-12 00:47:15 +0000

    Textbook.java works

--- unit11/Textbook.java
+++ unit11/Textbook.java
@@ -27,7 +27,30 @@ class Book {
     // shown.
 }
 
-// TODO write class Textbook
+class Textbook extends Book{
+    private int edition;
+    public Textbook(String a, double b, int c){
+        super(a,b);
+        edition = c;
+    }
+    public String getBookInfo(){
+        String a = super.getBookInfo();
+        a += "-" + edition;
+        return a;
+    }
+    public int getEdition(){
+        return edition;
+    }
+    public boolean canSubstituteFor(Textbook a){
+        if(getTitle().equals(a.getTitle())){
+            if(getEdition()>=a.getEdition()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
 
 class TextbookRun {
 
@@ -37,17 +60,15 @@ class TextbookRun {
     }
 
     public static void main(String[] args) {
-        // uncomment block when ready to test. Select, then Ctrl+/
-
-        // Textbook bio2015 = new Textbook("Biology", 49.75, 2);
-        // Textbook bio2019 = new Textbook("Biology", 39.75, 3);
-        // check(bio2019.getEdition() == 3);
-        // check(bio2019.getBookInfo().equals("Biology-39.75-3"));
-        // check(bio2019.canSubstituteFor(bio2015));
-        // check(!bio2015.canSubstituteFor(bio2019));
-        // Textbook math = new Textbook("Calculus", 45.25, 1);
-        // check(!bio2015.canSubstituteFor(math));
-        // System.out.println("Happy Panda! \uD83D\uDC3C");
+        Textbook bio2015 = new Textbook("Biology", 49.75, 2);
+        Textbook bio2019 = new Textbook("Biology", 39.75, 3);
+        check(bio2019.getEdition() == 3);
+        check(bio2019.getBookInfo().equals("Biology-39.75-3"));
+        check(bio2019.canSubstituteFor(bio2015));
+        check(!bio2015.canSubstituteFor(bio2019));
+        Textbook math = new Textbook("Calculus", 45.25, 1);
+        check(!bio2015.canSubstituteFor(math));
+        System.out.println("Happy Panda! \uD83D\uDC3C");
 
     }
 }
