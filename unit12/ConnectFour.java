d6a282daad398c770ebc7100ddb645f52362e8c1
NDD53 <126294583+NDD53@users.noreply.github.com>
2023-05-13 20:18:27 +0000

    It doesn't work, will find bug later

--- unit12/ConnectFour.java
+++ unit12/ConnectFour.java
@@ -53,22 +53,257 @@ public class ConnectFour {
         }
         return moves;
     }
-    
-    public void makeMove(int row, int col) {
-        //interfaces or somthing idk    
+
+    public int[][] makeMove() {
+        int[] plays = getMoves();
+        int col = findBestMove();
+        int[][] duplicate = board;
+        duplicate[plays[col]][col] = 1;
+        return duplicate;
+    }
+
+    public void setBoard(int[][] newBoard) {
+        board = newBoard;
+    }
+
+    /**
+     * @return the column of the best move
+     */
+    public int findBestMove() {
+        int[] plays = getMoves();
+        int max = Integer.MIN_VALUE;
+        int col = Integer.MIN_VALUE;
+        for (int i = 0; i < plays.length; i++) {
+            if (plays[i] != -1) {
+                if (getPlayScore(plays[i], i) > max) {
+                    max = getPlayScore(plays[i], i);
+                    col = i;
+                }
+            }
+        }
+        return col;
+    }
+
+    public int getPlayScore(int row, int col) {
+        int[][] duplicate = board;
+        duplicate[row][col] = 1;
+        return getBoardScore(duplicate);
+    }
+
+    public int getBoardScore(int[][] theBoard) {
+        if (fourInARow(theBoard)) {
+            return Integer.MAX_VALUE;
+        }
+        int score = 0;
+        score += getConsecutive(theBoard, 1, 1);
+        score -= getConsecutive(theBoard, 2, 1);
+        score += getConsecutive(theBoard, 1, 2) * 4;
+        score -= getConsecutive(theBoard, 2, 2) * 4;
+        score += getConsecutive(theBoard, 1, 2) * 16;
+        score -= getConsecutive(theBoard, 2, 2) * 16;
+        return score;
+    }
+
+    /**
+     * @param theBoard
+     * @return true if there is a four in a row of 1s
+     */
+    public boolean fourInARow(int[][] theBoard) {
+        // top bottom
+        for (int row = 0; row < theBoard.length - 3; row++) {
+            for (int col = 0; col < theBoard[0].length; col++) {
+                boolean test = false;
+                for (int i = 0; i < 4; i++) {
+                    if (theBoard[row + i][col] != 1) {
+                        test = false;
+                        break;
+                    }
+                }
+                if (test) {
+                    return true;
+                }
+            }
+        }
+        // left right
+        for (int row = 0; row < theBoard.length; row++) {
+            for (int col = 0; col < theBoard[0].length - 3; col++) {
+                boolean test = false;
+                for (int i = 0; i < 4; i++) {
+                    if (theBoard[row][col + 1] != 1) {
+                        test = false;
+                        break;
+                    }
+                }
+                if (test) {
+                    return true;
+                }
+            }
+        }
+        // bottom left to top right
+        for (int row = 3; row < theBoard.length; row++) {
+            for (int col = 0; col < theBoard[0].length - 3; col++) {
+                boolean test = false;
+                for (int i = 0; i < 4; i++) {
+                    if (theBoard[row - 1][col + 1] != 1) {
+                        test = false;
+                        break;
+                    }
+                }
+                if (test) {
+                    return true;
+                }
+            }
+        }
+        // bottom right to top left
+        for (int row = 0; row < theBoard.length - 3; row++) {
+            for (int col = 0; col < theBoard[0].length - 3; col++) {
+                boolean test = false;
+                for (int i = 0; i < 4; i++) {
+                    if (theBoard[row + 1][col + 1] != 1) {
+                        test = false;
+                        break;
+                    }
+                }
+                if (test) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * very untested
+     * 
+     * @param theBoard
+     * @param player   - either 1 or 2
+     * @param number   - the desired number of consecutive 1s or 2s followed by an
+     *                 empty space
+     * @return - the count of consecutive 1s or 2s followed by an empty space
+     */
+    public int getConsecutive(int[][] theBoard, int player, int number) {
+        int count = 0;
+        // bottom up
+        for (int row = number; row < theBoard.length; row++) {
+            for (int col = 0; col < theBoard[0].length; col++) {
+                boolean test = false;
+                for (int i = 0; i < number; i++) {
+                    if (theBoard[row - i][col] != player) {
+                        test = false;
+                        break;
+                    }
+                }
+                if (test && theBoard[row - number][col] == 0) {
+                    count++;
+                }
+            }
+        }
+        // right to left
+        for (int row = 0; row < theBoard.length; row++) {
+            for (int col = number; col < theBoard[0].length; col++) {
+                boolean test = false;
+                for (int i = 0; i < number; i++) {
+                    if (theBoard[row][col - i] != player) {
+                        test = false;
+                        break;
+                    }
+                }
+                if (test && theBoard[row][col - number] == 0) {
+                    count++;
+                }
+            }
+        }
+        // left to right
+        for (int row = 0; row < theBoard.length; row++) {
+            for (int col = 0; col < theBoard[0].length - number; col++) {
+                boolean test = false;
+                for (int i = 0; i < number; i++) {
+                    if (theBoard[row][col + i] != player) {
+                        test = false;
+                        break;
+                    }
+                }
+                if (test && theBoard[row][col + number] == 0) {
+                    count++;
+                }
+            }
+        }
+        // top left to bottom right
+        for (int row = 0; row < theBoard.length - number; row++) {
+            for (int col = 0; col < theBoard[0].length - number; col++) {
+                boolean test = false;
+                for (int i = 0; i < number; i++) {
+                    if (theBoard[row + i][col + i] != player) {
+                        test = false;
+                        break;
+                    }
+                }
+                if (test && theBoard[row + number][col + number] == 0) {
+                    count++;
+                }
+            }
+        }
+        // top right to bottom left
+        for (int row = 0; row < theBoard.length - number; row++) {
+            for (int col = number; col < theBoard[0].length; col++) {
+                boolean test = false;
+                for (int i = 0; i < number; i++) {
+                    if (theBoard[row + i][col - i] != player) {
+                        test = false;
+                        break;
+                    }
+                }
+                if (test && theBoard[row + number][col - number] == 0) {
+                    count++;
+                }
+            }
+        }
+        // bottom right to top left
+        for (int row = number; row < theBoard.length; row++) {
+            for (int col = number; col < theBoard[0].length; col++) {
+                boolean test = false;
+                for (int i = 0; i < number; i++) {
+                    if (theBoard[row - i][col - i] != player) {
+                        test = false;
+                        break;
+                    }
+                }
+                if (test && theBoard[row - number][col - number] == 0) {
+                    count++;
+                }
+            }
+        }
+        // bottom left to top right
+        for (int row = number; row < theBoard.length; row++) {
+            for (int col = 0; col < theBoard[0].length - number; col++) {
+                boolean test = false;
+                for (int i = 0; i < number; i++) {
+                    if (theBoard[row - i][col + i] != player) {
+                        test = false;
+                        break;
+                    }
+                }
+                if (test && theBoard[row - number][col + number] == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
 
 class Runner {
     public static void main(String[] args) {
         int[][] board = { { 0, 0, 2, 0, 0, 0 }, { 0, 0, 1, 0, 0, 1 }, { 0, 0, 1, 0, 0, 1 }, { 0, 0, 2, 0, 1, 2 },
-                { 1, 0, 1, 0, 2, 2}, { 2, 0, 1, 2, 1, 2 } };
+                { 1, 0, 1, 0, 2, 2 }, { 2, 0, 2, 2, 1, 2 } };
         ConnectFour game = new ConnectFour(board);
-        int[] moves = game.getMoves();
+        int[][] newBoard = game.makeMove();
         for (int[] a : board) {
             System.out.println(Arrays.toString(a));
         }
         System.out.println("~~~~~~~~~~~~~~~~~~");
-        System.out.println(Arrays.toString(moves));
+        for (int[] a : newBoard) {
+            System.out.println(Arrays.toString(a));
+        }
     }
 }
