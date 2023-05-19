352ed44cbffbdd604ce174b6ba330886c30f9d84
NDD53 <126294583+NDD53@users.noreply.github.com>
2023-05-19 05:56:58 +0000

    Part 1 works!!!! I used regular expressions!!!!!!!

--- unit12/SFFT.java
+++ unit12/SFFT.java
@@ -2,6 +2,8 @@ package unit12;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SFFT {
     // in the form axy + bx + cy + d
@@ -10,37 +12,40 @@ public class SFFT {
     private String problem;
     private ArrayList<String> solution;
     private String user;
+    private Scanner scanner;
+
     public SFFT(int level, Scanner scanner) {
         this.level = level;
-        generate(scanner);
-    }
-
-    public void generate(Scanner scanner){
-        if(level==1){
-            a = 1;
-            b = (int)(Math.random()*6)+4;
-            c = (int)(Math.random()*6)+4;
-            d = b*c;
-            problem = "factor \"xy + " + b + "x + " + c + "y + " + d + "\"";
-            System.out.println(problem);
-            user = scanner.nextLine();
-            // 2(x+6)(y+7)
-            // 2*(x+6)(y+7)
-            // (x+6)*2*(y+7)
-            // (x+6)2(y+7)
-            // (x + 6)(y + 7)
-            // (x +6)(y+ 7)
-            // (x + 6)*(y + 7)
-            // (y+7)(x+6)
-            // (     y+7)(x+6    )
-            clean();
+        this.scanner = scanner;
+        if (level == 1) {
+            generateL1();
         }
     }
 
-    public void clean(){
+    public void generateL1() {
+        a = 1;
+        b = (int) (Math.random() * 6) + 4;
+        c = (int) (Math.random() * 6) + 4;
+        d = b * c;
+        problem = "factor \"xy + " + b + "x + " + c + "y + " + d + "\"";
+        System.out.println(problem);
+        user = scanner.nextLine();
+        clean();
+        Pattern pattern = Pattern.compile("((([(][x][+][" + c + "][)])|([(][" + c + "][+][x][)]))(([(][y][+][" + b
+                + "][)])|([(][" + b + "][+][y][)])))|((([(][y][+][" + b + "][)])|([(][" + b + "][+][y][)]))(([(][x][+]["
+                + c + "][)])|([(][" + c + "][+][x][)])))");
+        Matcher matcher = pattern.matcher(user);
+        boolean matchFound = matcher.find();
+        if (matchFound) {
+            System.out.println("Correct");
+        } else {
+            System.out.println("Incorrect");
+        }
+    }
+
+    public void clean() {
         user = user.replaceAll(" ", "");
         user = user.replaceAll("\\*", "");
-        System.out.println(user);
     }
 }
 
@@ -48,6 +53,6 @@ class SFFTRunner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         SFFT sfft = new SFFT(1, scanner);
-        scanner.close();   
+        scanner.close();
     }
 }
